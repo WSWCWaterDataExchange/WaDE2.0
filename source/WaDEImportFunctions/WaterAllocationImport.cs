6d761099be139ce453a942b22a13b8f141f164b6
Jesse Temple <jtemple@dontpaniclabs.com>
2019-04-19 16:03:27 -0500

    implemented import functions

--- source/WaDEImportFunctions/WaterAllocationImport.cs
+++ source/WaDEImportFunctions/WaterAllocationImport.cs
@@ -1,15 +1,12 @@
-using System;
-using System.IO;
-using System.Net.Http;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using System.Threading.Tasks;
 using WesternStatesWater.WaDE.Contracts.Import;
 
 namespace WaDEImportFunctions
@@ -40,7 +37,7 @@ namespace WaDEImportFunctions
 
             var results = await Task.WhenAll(parallelTasks);
 
-            foreach(var resultItem in results)
+            foreach (var resultItem in results)
             {
                 log.LogInformation(JsonConvert.SerializeObject(resultItem));
             }
@@ -64,26 +61,85 @@ namespace WaDEImportFunctions
         public async Task<StatusHelper> LoadOrganizations([ActivityTrigger] DurableActivityContextBase context, ILogger log)
         {
             var runId = context.GetInput<string>();
-            var result = new StatusHelper { Name= FunctionNames.LoadOrganizations, Status = await WaterAllocationManager.LoadOrganizations(runId) };
+            var result = new StatusHelper { Name = FunctionNames.LoadOrganizations, Status = await WaterAllocationManager.LoadOrganizations(runId) };
             log.LogInformation(JsonConvert.SerializeObject(result));
+            return result;
+        }
+
+        [FunctionName(FunctionNames.LoadAggregatedAmounts)]
+        public async Task<StatusHelper> LoadAggregatedAmounts([ActivityTrigger] DurableActivityContextBase context, ILogger log)
+        {
+            var runId = context.GetInput<string>();
+            var result = new StatusHelper { Name = FunctionNames.LoadOrganizations, Status = await WaterAllocationManager.LoadAggregatedAmounts(runId) };
+
+            log.LogInformation(JsonConvert.SerializeObject(result));
+
+            return result;
+        }
+
+        [FunctionName(FunctionNames.LoadRegulatoryOverlays)]
+        public async Task<StatusHelper> LoadRegulatoryOverlays([ActivityTrigger] DurableActivityContextBase context, ILogger log)
+        {
+            var runId = context.GetInput<string>();
+            var result = new StatusHelper { Name = FunctionNames.LoadOrganizations, Status = await WaterAllocationManager.LoadRegulatoryOverlays(runId) };
+
+            log.LogInformation(JsonConvert.SerializeObject(result));
+
+            return result;
+        }
+
+        [FunctionName(FunctionNames.LoadReportingUnits)]
+        public async Task<StatusHelper> LoadReportingUnits([ActivityTrigger] DurableActivityContextBase context, ILogger log)
+        {
+            var runId = context.GetInput<string>();
+            var result = new StatusHelper { Name = FunctionNames.LoadOrganizations, Status = await WaterAllocationManager.LoadReportingUnits(runId) };
+
+            log.LogInformation(JsonConvert.SerializeObject(result));
+
             return result;
         }
 
         [FunctionName(FunctionNames.LoadSites)]
         public async Task<StatusHelper> LoadSites([ActivityTrigger] DurableActivityContextBase context, ILogger log)
         {
-            await Task.Delay(TimeSpan.FromSeconds(5));
-            var result = new StatusHelper { Name = FunctionNames.LoadSites, Status = true };
+            var runId = context.GetInput<string>();
+            var result = new StatusHelper { Name = FunctionNames.LoadSites, Status = await WaterAllocationManager.LoadSites(runId) };
+
             log.LogInformation(JsonConvert.SerializeObject(result));
+
+            return result;
+        }
+
+        [FunctionName(FunctionNames.LoadSiteSpecificAmounts)]
+        public async Task<StatusHelper> LoadSiteSpecificAmounts([ActivityTrigger] DurableActivityContextBase context, ILogger log)
+        {
+            var runId = context.GetInput<string>();
+            var result = new StatusHelper { Name = FunctionNames.LoadSites, Status = await WaterAllocationManager.LoadSiteSpecificAmounts(runId) };
+
+            log.LogInformation(JsonConvert.SerializeObject(result));
+
+            return result;
+        }
+
+        [FunctionName(FunctionNames.LoadVariables)]
+        public async Task<StatusHelper> LoadVariables([ActivityTrigger] DurableActivityContextBase context, ILogger log)
+        {
+            var runId = context.GetInput<string>();
+            var result = new StatusHelper { Name = FunctionNames.LoadSites, Status = await WaterAllocationManager.LoadVariables(runId) };
+
+            log.LogInformation(JsonConvert.SerializeObject(result));
+
             return result;
         }
 
         [FunctionName(FunctionNames.LoadWaterSources)]
         public async Task<StatusHelper> LoadWaterSources([ActivityTrigger] DurableActivityContextBase context, ILogger log)
         {
-            await Task.Delay(TimeSpan.FromSeconds(5));
-            var result = new StatusHelper { Name = FunctionNames.LoadWaterSources, Status = true };
+            var runId = context.GetInput<string>();
+            var result = new StatusHelper { Name = FunctionNames.LoadWaterSources, Status = await WaterAllocationManager.LoadWaterSources(runId) };
+
             log.LogInformation(JsonConvert.SerializeObject(result));
+
             return result;
         }
 
@@ -99,9 +155,11 @@ namespace WaDEImportFunctions
         [FunctionName(FunctionNames.LoadMethods)]
         public async Task<StatusHelper> LoadMethods([ActivityTrigger] DurableActivityContextBase context, ILogger log)
         {
-            await Task.Delay(TimeSpan.FromSeconds(5));
-            var result = new StatusHelper { Name = FunctionNames.LoadMethods, Status = true };
+            var runId = context.GetInput<string>();
+            var result = new StatusHelper { Name = FunctionNames.LoadMethods, Status = await WaterAllocationManager.LoadMethods(runId) };
+
             log.LogInformation(JsonConvert.SerializeObject(result));
+
             return result;
         }
 
@@ -164,5 +222,10 @@ namespace WaDEImportFunctions
         public const string LoadVariablesSpecific = "LoadVariablesSpecific";
         public const string LoadMethods = "LoadMethods";
         public const string LoadWaterAllocation = "LoadWaterAllocation";
+        public const string LoadAggregatedAmounts = "LoadAggregatedAmounts";
+        public const string LoadRegulatoryOverlays = "LoadRegulatoryOverlays";
+        public const string LoadReportingUnits = "LoadReportingUnits";
+        public const string LoadSiteSpecificAmounts = "LoadSiteSpecificAmounts";
+        public const string LoadVariables = "LoadVariables";
     }
 }
