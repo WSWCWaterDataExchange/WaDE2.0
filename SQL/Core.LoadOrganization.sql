72334c633b535c7480802c7ca318147d50dbad47
Jesse Temple <jtemple@dontpaniclabs.com>
2019-04-30 13:58:18 -0500

    updated for readability

--- SQL/Core.LoadOrganization.sql
+++ SQL/Core.LoadOrganization.sql
@@ -1,80 +1,86 @@
-USE [WaDE2]
-GO
-/****** Object:  StoredProcedure [Core].[LoadOrganization]    Script Date: 4/17/2019 11:28:45 AM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-
 ALTER PROCEDURE [Core].[LoadOrganization]
 (
-     @RunId nvarchar(250),
-     @OrganizationTable Core.OrganizationTableType READONLY
+	@RunId NVARCHAR(250),
+	@OrganizationTable Core.OrganizationTableType READONLY
 )
 AS
 BEGIN
-  SELECT ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) RowNumber, *
-    INTO #TempOrganizationData
-    FROM @OrganizationTable;
+	SELECT ROW_NUMBER() OVER(ORDER BY (SELECT NULL)) RowNumber, *
+	INTO #TempOrganizationData
+	FROM @OrganizationTable;
 
+	--data validation
+	WITH q1 AS
+	(
+		SELECT 'OranizationUUID Not Valid' Reason, *
+		FROM #TempOrganizationData
+		WHERE OrganizationUUID IS NULL
+		UNION ALL
+		SELECT 'OrganizationName Not Valid' Reason, *
+		FROM #TempOrganizationData
+		WHERE OrganizationName IS NULL
+		UNION ALL
+		SELECT 'OrganizationWebsite Not Valid' Reason, *
+		FROM #TempOrganizationData
+		WHERE OrganizationWebsite IS NULL
+		UNION ALL
+		SELECT 'OrganizationPhoneNumber Not Valid' Reason, *
+		FROM #TempOrganizationData
+		WHERE OrganizationPhoneNumber IS NULL
+		UNION ALL
+		SELECT 'OrganizationContactName Not Valid' Reason, *
+		FROM #TempOrganizationData
+		WHERE OrganizationContactName IS NULL
+		UNION ALL
+		SELECT 'OrganizationContactEmail Not Valid' Reason, *
+		FROM #TempOrganizationData
+		WHERE OrganizationContactEmail IS NULL
+		UNION ALL
+		SELECT 'DataMappingURL Not Valid' Reason, *
+		FROM #TempOrganizationData
+		WHERE DataMappingURL IS NULL
+	)
+	SELECT * INTO #TempErrorOrganizationRecords FROM q1;
 
-WITH q1 as (
-  SELECT 'OranizationUUID Not Valid' Reason, *
-    FROM #TempOrganizationData
-	WHERE OrganizationUUID is null
-  UNION ALL
-  SELECT 'OrganizationName Not Valid' Reason, *
-	FROM #TempOrganizationData
-	WHERE OrganizationName is null
-  UNION ALL
-  SELECT 'OrganizationWebsite Not Valid' Reason, *
-	FROM #TempOrganizationData
-	WHERE OrganizationWebsite is null
-  UNION ALL
-  SELECT 'OrganizationPhoneNumber Not Valid' Reason, *
-	FROM #TempOrganizationData
-	WHERE OrganizationPhoneNumber is null
-  UNION ALL
-  SELECT 'OrganizationContactName Not Valid' Reason, *
-	FROM #TempOrganizationData
-	WHERE OrganizationContactName is null
-  UNION ALL
-  SELECT 'OrganizationContactEmail Not Valid' Reason, *
-	FROM #TempOrganizationData
-	WHERE OrganizationContactEmail is null
-	UNION ALL
-  SELECT 'DataMappingURL Not Valid' Reason, *
-	FROM #TempOrganizationData
-	WHERE DataMappingURL is null)
-  SELECT *
-    INTO #TempErrorOrganizationRecords
-	FROM q1;
-
-  IF exists(select 1 from #TempErrorOrganizationRecords) 
-  BEGIN
-    insert into Core.ImportErrors ([Type], [RunId], [Data])
+	--if we have errors, insert them and bail out
+	IF EXISTS(SELECT 1 FROM #TempErrorOrganizationRecords) 
+	BEGIN
+		INSERT INTO Core.ImportErrors ([Type], [RunId], [Data])
 	  
-	  VALUES ('Organizations', @RunId, (select * from #TempErrorOrganizationRecords FOR JSON PATH));
-    return 1;
-  END
+		VALUES ('Organizations', @RunId, (SELECT * FROM #TempErrorOrganizationRecords FOR JSON PATH));
+		RETURN 1;
+	END
 
-
-
-
-  
-    MERGE INTO CORE.Organizations_dim as Target USING #TempOrganizationData as Source on Target.OrganizationUUID = Source.OrganizationUUID
-      WHEN MATCHED THEN
-	    UPDATE SET OrganizationName = Source.OrganizationName,
-		           OrganizationPurview = Source.OrganizationPurview,
-				   OrganizationWebsite = Source.OrganizationWebsite,
-				   OrganizationPhoneNumber = Source.OrganizationPhoneNumber,
-				   OrganizationContactName = Source.OrganizationContactName,
-				   OrganizationContactEmail = Source.OrganizationContactEmail,
-				   DataMappingURL = Source.DataMappingURL
-      WHEN NOT MATCHED THEN
-	    INSERT (OrganizationUUID, OrganizationName, OrganizationPurview, OrganizationWebsite, OrganizationPhoneNumber, OrganizationContactName, OrganizationContactEmail, DataMappingURL)
-		  VALUES (Source.OrganizationUUID, Source.OrganizationName, Source.OrganizationPurview, Source.OrganizationWebsite, Source.OrganizationPhoneNumber, Source.OrganizationContactName, Source.OrganizationContactEmail, Source.DataMappingURL);
-
-  return 0;
+	--merge the data
+    MERGE INTO CORE.Organizations_dim AS Target USING #TempOrganizationData AS Source
+		ON Target.OrganizationUUID = Source.OrganizationUUID
+	WHEN MATCHED THEN
+		UPDATE SET
+			OrganizationName = Source.OrganizationName
+			,OrganizationPurview = Source.OrganizationPurview
+			,OrganizationWebsite = Source.OrganizationWebsite
+			,OrganizationPhoneNumber = Source.OrganizationPhoneNumber
+			,OrganizationContactName = Source.OrganizationContactName
+			,OrganizationContactEmail = Source.OrganizationContactEmail
+			,DataMappingURL = Source.DataMappingURL
+	WHEN NOT MATCHED THEN
+		INSERT
+			(OrganizationUUID
+			,OrganizationName
+			,OrganizationPurview
+			,OrganizationWebsite
+			,OrganizationPhoneNumber
+			,OrganizationContactName
+			,OrganizationContactEmail
+			,DataMappingURL)
+		VALUES
+			(Source.OrganizationUUID
+			,Source.OrganizationName
+			,Source.OrganizationPurview
+			,Source.OrganizationWebsite
+			,Source.OrganizationPhoneNumber
+			,Source.OrganizationContactName
+			,Source.OrganizationContactEmail
+			,Source.DataMappingURL);
+	RETURN 0;
 END
