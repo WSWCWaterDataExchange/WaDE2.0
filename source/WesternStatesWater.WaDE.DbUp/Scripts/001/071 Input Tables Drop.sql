8e07b7de77162074313674c2e759b9b60ebc8cd3
Trang Do <tdo@dontpaniclabs.com>
2019-10-25 16:35:48 -0500

    initial commit for many to many connection and dropping input tables

---
+++ source/WesternStatesWater.WaDE.DbUp/Scripts/001/071 Input Tables Drop.sql
@@ -0,0 +1,14 @@
+﻿DROP TABLE IF EXISTS 
+	[Input].[AggregatedAmounts_fact],
+	[Input].[AllocationAmounts_fact],
+	[Input].[BeneficialUses_dim],
+	[Input].[Methods_dim],
+	[Input].[Organizations_dim],
+	[Input].[RegulatoryOverlay_dim],
+	[Input].[RegulatoryReportingUnits_fact],
+	[Input].[ReportingUnits_dim],
+	[Input].[Sites_dim],
+	[Input].[SiteVariableAmounts_fact],
+	[Input].[Variables_dim],
+	[Input].[WaterSources_dim]
+GO
