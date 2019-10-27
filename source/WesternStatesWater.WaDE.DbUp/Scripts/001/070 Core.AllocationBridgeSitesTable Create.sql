8fd2251e263664b3e0dd26185337f5803e02789f
Trang Do <tdo@dontpaniclabs.com>
2019-10-27 16:59:07 -0500

    update create table to add foreign keys

--- source/WesternStatesWater.WaDE.DbUp/Scripts/001/070 Core.AllocationBridgeSitesTable Create.sql
+++ source/WesternStatesWater.WaDE.DbUp/Scripts/001/070 Core.AllocationBridgeSitesTable Create.sql
@@ -7,3 +7,13 @@ CREATE TABLE [Core].[AllocationBridge_Sites_fact](
 	[AllocationBridgeID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+
+ALTER TABLE [Core].[AllocationBridge_Sites_fact]  WITH CHECK ADD  CONSTRAINT [FK_AllocationBridge_Sites_fact_AllocationAmounts_fact] FOREIGN KEY([AllocationAmountID])
+REFERENCES [Core].[AllocationAmounts_fact] ([AllocationAmountID])
+
+ALTER TABLE [Core].[AllocationBridge_Sites_fact] CHECK CONSTRAINT [FK_AllocationBridge_Sites_fact_AllocationAmounts_fact]
+
+ALTER TABLE [Core].[AllocationBridge_Sites_fact]  WITH CHECK ADD  CONSTRAINT [FK_AllocationBridge_Sites] FOREIGN KEY([SiteID])
+REFERENCES [Core].[Sites_Dim] ([SiteID])
+
+ALTER TABLE [Core].[AllocationBridge_Sites_fact] CHECK CONSTRAINT [FK_AllocationBridge_Sites]
