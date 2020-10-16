ea03174b7f22d977d9d031990b23c591438522c6
Zach Lannin <zslannin@outlook.com>
2020-10-16 15:20:03 -0500

    Create 002 CVs.WaDEName.sql

---
+++ source/WesternStatesWater.WaDE.DbUp/Scripts/004/002 CVs.WaDEName.sql
@@ -0,0 +1,34 @@
+﻿DECLARE @SchemaId int
+DECLARE @CvTables TABLE ([name] nvarchar(max))
+DECLARE @Name nvarchar(max)
+DECLARE @GetName CURSOR
+DECLARE @SQL nvarchar(max)
+
+SET @SchemaId = (
+	SELECT SCHEMA_ID 
+	FROM sys.schemas 
+	WHERE name = 'CVs'
+)
+
+INSERT INTO @CvTables
+SELECT name
+FROM sys.tables
+WHERE schema_id = @SchemaId
+
+SET @GetName = CURSOR FOR
+SELECT [name]
+FROM @CvTables
+
+OPEN @GetName
+FETCH NEXT
+FROM @GetName into @Name
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	SET @SQL = N'ALTER TABLE [CVs.[' + @Name + '] ADD [WaDEName] NVARCHAR(150) NULL'
+    PRINT @SQL
+	EXEC Sp_executesql @SQL
+	FETCH NEXT
+	FROM @GetName INTO @Name
+END
+
+CLOSE @GetName
