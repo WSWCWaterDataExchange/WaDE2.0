d1f5084276ea817d0e22e16b2ee9dec5bc3752fd
Jeff Hastings <jhastings@dontpaniclabs.com>
2021-05-03 10:47:22 -0500

    adding script for new fields

---
+++ source/WesternStatesWater.WaDE.DbUp/Scripts/005/001 Core.RegulatoryOverlay_dim.sql
@@ -0,0 +1,11 @@
+﻿ALTER TABLE [Core].[RegulatoryOverlay_dim]
+ADD [ApplicableWaterSourceTypeCV] NVARCHAR(50) NULL 
+CONSTRAINT FK_RegulatoryOverlay_dim_WaterSourceTypeCV FOREIGN KEY (WatherSourceTypeCV)
+REFERENCES [CVs].[WaterSourceType] ([Name])
+GO
+
+ALTER TABLE [Core].[RegulatoryOverlay_dim]
+ADD [RegulationTypeCV] NVARCHAR (50) NULL
+CONSTRAINT FK_RegulatoryOverlay_dim_RegulationTypeCV FOREIGN KEY (RegulationTypeCV)
+REFERENCES [CVs].[RegulationTypeCV] ([Name])
+GO
