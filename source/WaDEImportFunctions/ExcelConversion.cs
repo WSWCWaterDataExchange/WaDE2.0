46d5a9f2ed14690a4e38dcec65106b3ad6dd562b
Yan Xin Lee <ylee@dontpaniclabs.com>
2021-05-03 11:36:41 -0500

    removed unused references

--- source/WaDEImportFunctions/ExcelConversion.cs
+++ source/WaDEImportFunctions/ExcelConversion.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using ManagerImport = WesternStatesWater.WaDE.Contracts.Import;
 
