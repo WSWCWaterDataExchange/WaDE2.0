961435b9bca7799c077a001c3ae8fa311302194f
Nathan Wilkinson <nwilkinson@dontpaniclabs.com>
2022-02-23 09:02:03 -0600

    Add support for faking AggregatedAmounts

--- source/WesternStatesWater.WaDE.Tests.Helpers/ModelBuilder/Accessor/Api/AggregatedAmountsOrganizationBuilder.cs
+++ source/WesternStatesWater.WaDE.Tests.Helpers/ModelBuilder/Accessor/Api/AggregatedAmountsOrganizationBuilder.cs
@@ -4,11 +4,11 @@ using WesternStatesWater.WaDE.Accessors.Contracts.Api;
 
 namespace WesternStatesWater.WaDE.Tests.Helpers.ModelBuilder.Accessor.Api
 {
-    public static class WaterAllocationOrganizationBuilder
+    public static class AggregatedAmountsOrganizationBuilder
     {
-        public static WaterAllocationOrganization Create()
+        public static AggregatedAmountsOrganization Create()
         {
-            var faker = new Faker<WaterAllocationOrganization>()
+            var faker = new Faker<AggregatedAmountsOrganization>()
                 .RuleFor(a => a.OrganizationId, f => f.Random.Long(1))
                 .RuleFor(a => a.OrganizationName, f => f.Company.CompanyName())
                 .RuleFor(a => a.OrganizationPurview, f => f.Random.Word())
@@ -18,12 +18,14 @@ namespace WesternStatesWater.WaDE.Tests.Helpers.ModelBuilder.Accessor.Api
                 .RuleFor(a => a.OrganizationContactEmail, f => f.Internet.Email())
                 .RuleFor(a => a.OrganizationState, f => f.Address.StateAbbr())
 
-                .RuleFor(a => a.WaterSources, f => new List<WaterSource> { WaterSourceBuilder.Create() })
                 .RuleFor(a => a.VariableSpecifics, f => new List<VariableSpecific> { VariableSpecificBuilder.Create() })
+                .RuleFor(a => a.ReportingUnits, f => new List<ReportingUnit> { ReportingUnitBuilder.Create() })
+                .RuleFor(a => a.WaterSources, f => new List<WaterSource> { WaterSourceBuilder.Create() })
                 .RuleFor(a => a.Methods, f => new List<Method> { MethodBuilder.Create() })
                 .RuleFor(a => a.BeneficialUses, f => new List<BeneficialUse> { BeneficialUseBuilder.Create() })
-                .RuleFor(a => a.WaterAllocations, f => new List<Allocation> { AllocationBuilder.Create() })
-                .RuleFor(a => a.RegulatoryOverlays, f => new List<RegulatoryOverlay> { RegulatoryOverlayBuilder.Create() });
+                .RuleFor(a => a.AggregatedAmounts, f => new List<AggregatedAmount> { AggregatedAmountBuilder.Create() })
+                .RuleFor(a => a.RegulatoryOverlays, f => new List<RegulatoryOverlay> { RegulatoryOverlayBuilder.Create() })
+                ;
 
             return faker;
         }
