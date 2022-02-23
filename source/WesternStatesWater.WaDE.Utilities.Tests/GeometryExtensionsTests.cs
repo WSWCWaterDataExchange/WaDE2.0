ba9bb49a693e691ec66953ac2f349fe9564b7e9e
Nathan Wilkinson <nwilkinson@dontpaniclabs.com>
2022-02-23 08:25:39 -0600

    Add utilities to get a geometry object from either a Wkt or GeoJson string

--- source/WesternStatesWater.WaDE.Utilities.Tests/GeometryExtensionsTests.cs
+++ source/WesternStatesWater.WaDE.Utilities.Tests/GeometryExtensionsTests.cs
@@ -14,7 +14,7 @@ public class GeometryExtensionsTests
     [DataRow("POINT(-96.7014 40.8146)", "{\"type\":\"Point\",\"coordinates\":[-96.7014,40.8146]}")]
     [DataRow("POLYGON((-96.7015 40.8149,-96.7012 40.8149,-96.7012 40.8146,-96.7015 40.8146,-96.7015 40.8149))", "{\"type\":\"Polygon\",\"coordinates\":[[[-96.7015,40.8149],[-96.7012,40.8149],[-96.7012,40.8146],[-96.7015,40.8146],[-96.7015,40.8149]]]}")]
     [DataRow("MULTIPOLYGON (((40 40, 20 45, 45 30, 40 40)),((20 35, 45 20, 30 5, 10 10, 10 30, 20 35),(30 20, 20 25, 20 15, 30 20)))", "{\"type\":\"MultiPolygon\",\"coordinates\":[[[[40.0,40.0],[20.0,45.0],[45.0,30.0],[40.0,40.0]]],[[[20.0,35.0],[45.0,20.0],[30.0,5.0],[10.0,10.0],[10.0,30.0],[20.0,35.0]],[[30.0,20.0],[20.0,25.0],[20.0,15.0],[30.0,20.0]]]]}")]
-    public void TestMethod1(string wkt, string expectedGeoJson)
+    public void AsGeoJson(string wkt, string expectedGeoJson)
     {
         Geometry? geometry = null;
         if (wkt != null)
@@ -26,5 +26,28 @@ public class GeometryExtensionsTests
 
         var result = geometry.AsGeoJson();
         result.Should().Be(expectedGeoJson);
+    }
+
+    [DataTestMethod]
+    [DataRow(null, null)]
+    [DataRow("", null)]
+    [DataRow(" ", null)]
+    [DataRow("\t", null)]
+    [DataRow("POLYGON ((-96.7015 40.8149,-96.7012 40.8149,-96.7012 40.8146,-96.7015 40.8146,-96.7015 40.8149))", "POLYGON ((-96.7015 40.8149,-96.7012 40.8149,-96.7012 40.8146,-96.7015 40.8146,-96.7015 40.8149))")]
+    [DataRow("{\"type\":\"Polygon\",\"coordinates\":[[[-96.7015,40.8149],[-96.7012,40.8149],[-96.7012,40.8146],[-96.7015,40.8146],[-96.7015,40.8149]]]}", "POLYGON ((-96.7015 40.8149,-96.7012 40.8149,-96.7012 40.8146,-96.7015 40.8146,-96.7015 40.8149))")]
+    public void GetGeometry(string value, string expectedWkt)
+    {
+        var result = GeometryExtensions.GetGeometry(value);
+        if (expectedWkt != null)
+        {
+            result.Should().NotBeNull();
+            result!.IsValid.Should().BeTrue();
+
+            result.EqualsNormalized(GeometryExtensions.GetGeometryByWkt(expectedWkt)).Should().BeTrue();
+        }
+        else
+        {
+            result.Should().BeNull();
+        }
     }
 }
