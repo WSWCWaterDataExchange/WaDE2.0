e6442fdfd842c05cbb7a37ca15ddba9d44bdda96
Nathan Wilkinson <nwilkinson@dontpaniclabs.com>
2022-02-24 10:32:59 -0600

    Add test for reading sites.csv

--- source/WesternStatesWater.WaDE.Accessors.Tests/ImportDataIngestionFileAccessor.cs
+++ source/WesternStatesWater.WaDE.Accessors.Tests/ImportDataIngestionFileAccessor.cs
@@ -29,6 +29,37 @@ namespace WesternStatesWater.WaDE.Accessors.Tests
             results[0].IrrigatedAcreage.Should().BeNull();
         }
 
+        [TestMethod]
+        public async Task GetSites_BasicDataRead()
+        {
+            var runId = (new Faker()).Random.AlphaNumeric(5);
+            FileStreamFactoryMock.Arrange(a => a.GetStreamAsync(Arg.IsAny<IConfiguration>(), runId, "sites.csv"))
+                .Returns(Task.FromResult((Stream)new MemoryStream(System.Text.Encoding.UTF8.GetBytes(ImportData.ImportData.Sites_OneRecord))));
+
+            var sut = CreateWaterAllocationFileAccessor();
+            var results = await sut.GetSites(runId, 0, 10000);
+
+            results.Should().HaveCount(1);
+            results[0].SiteUUID.Should().Be("NEwr_S1");
+            results[0].RegulatoryOverlayUUIDs.Should().Be("abcd");
+            results[0].WaterSourceUUIDs.Should().Be("NEwr_WS1,NEwr_WS2");
+            results[0].CoordinateAccuracy.Should().Be("1.01");
+            results[0].CoordinateMethodCV.Should().Be("Unspecified");
+            results[0].County.Should().Be("Thayer");
+            results[0].EPSGCodeCV.Should().Be("4326");
+            results[0].Geometry.Should().Be("Fake Geometry");
+            results[0].GNISCodeCV.Should().Be("999");
+            results[0].HUC12.Should().Be("103000000000");
+            results[0].HUC8.Should().Be("158400000000");
+            results[0].Latitude.Should().Be("40.01104072");
+            results[0].Longitude.Should().Be("-97.5223804");
+            results[0].PODorPOUSite.Should().Be("POD");
+            results[0].SiteName.Should().Be("Unspecified");
+            results[0].SiteNativeID.Should().Be("10443");
+            results[0].SiteTypeCV.Should().Be("Unspecified");
+            results[0].USGSSiteID.Should().Be("Fake USGS");
+        }
+
         public IDataIngestionFileAccessor CreateWaterAllocationFileAccessor()
         {
             return new DataIngestionFileAccessor(Configuration.GetConfiguration(), null)
