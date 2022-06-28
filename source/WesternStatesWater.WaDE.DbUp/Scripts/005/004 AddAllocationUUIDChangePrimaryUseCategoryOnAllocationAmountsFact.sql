8e2d60c0785e1b3f32eba9b71c4cd5b133a6322a
Sarah Kenny <skenny@dontpaniclabs.com>
2022-06-28 10:53:39 -0500

    Adding print statements for debugging

--- source/WesternStatesWater.WaDE.DbUp/Scripts/005/004 AddAllocationUUIDChangePrimaryUseCategoryOnAllocationAmountsFact.sql
+++ source/WesternStatesWater.WaDE.DbUp/Scripts/005/004 AddAllocationUUIDChangePrimaryUseCategoryOnAllocationAmountsFact.sql
@@ -1,17 +1,26 @@
 DELETE FROM [Core].[AllocationBridge_BeneficialUses_fact];
+PRINT 'all values deleted from allocationBridgeBeneficialUsesFact';
 DELETE FROM [Core].[AllocationBridge_Sites_fact];
+PRINT 'all values deleted from allocationBridgeSitesFact';
 DELETE FROM [Core].[AllocationAmounts_fact];
+PRINT 'all values deleted from allocationAmountsFact';
 ALTER TABLE [Core].[AllocationAmounts_fact] Add AllocationUUID NVARCHAR (250) NOT NULL;
+PRINT 'added allocationUUID';
 GO
+PRINT 'GO';
 
 EXEC sp_rename '[Core].[AllocationAmounts_fact].PrimaryUseCategoryCV', 'PrimaryBeneficialUseCategory', 'COLUMN';
+PRINT 'rename column primaryBeneficialUseCategory';
 ALTER TABLE [Core].[AllocationAmounts_fact] DROP CONSTRAINT FK_AllocationAmounts_BeneficialUses;
+PRINT 'drop beneficialUses constraint';
 ALTER TABLE Core.AllocationAmounts_fact
 ALTER COLUMN PrimaryBeneficialUseCategory NVARCHAR(150) NULL;
+PRINT 'change primaryBeneficial to nvarchar(150) null';
 CREATE UNIQUE INDEX IX_AllocationUUID on [Core].[AllocationAmounts_fact] (AllocationUUID);
+PRINT 'create allocationUUID unique index';
 GO
+PRINT 'GO';
 
-/****** Object:  UserDefinedTableType [Core].[WaterAllocationTableType]    Script Date: 6/13/2022 3:14:49 PM ******/
 CREATE TYPE [Core].[WaterAllocationTableType_new] AS TABLE(
 	[OrganizationUUID] [nvarchar](250) NULL,
 	[VariableSpecificUUID] [nvarchar](250) NULL,
@@ -55,5 +64,9 @@ CREATE TYPE [Core].[WaterAllocationTableType_new] AS TABLE(
 )
 GO
 
+PRINT 'created new table type';
+
 EXEC Core.UpdateUUDT 'Core', 'WaterAllocationTableType';
 GO
+PRINT 'replaced table type';
+
