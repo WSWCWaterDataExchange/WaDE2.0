362539075d70832e2c2d7db2338af2a20e1176d8
Sarah Kenny <skenny@dontpaniclabs.com>
2022-06-28 12:26:02 -0500

    moving table type

--- source/WesternStatesWater.WaDE.DbUp/Scripts/005/005 AllocationUUIDAndPrimaryUseCategoryStoredProcedure.sql
+++ source/WesternStatesWater.WaDE.DbUp/Scripts/005/005 AllocationUUIDAndPrimaryUseCategoryStoredProcedure.sql
@@ -1,3 +1,53 @@
+
+CREATE TYPE [Core].[WaterAllocationTableType_new] AS TABLE(
+	[OrganizationUUID] [nvarchar](250) NULL,
+	[VariableSpecificUUID] [nvarchar](250) NULL,
+	[SiteUUID] [nvarchar](max) NULL,
+	[MethodUUID] [nvarchar](250) NULL,
+	[BeneficialUseCategory] [nvarchar](500) NULL,
+	[PrimaryBeneficialUseCategory] [nvarchar](150) NULL,
+	[DataPublicationDATE] [date] NULL,
+	[DataPublicationDOI] [nvarchar](100) NULL,
+	[AllocationNativeID] [nvarchar](250) NULL,
+	[AllocationApplicationDate] [date] NULL,
+	[AllocationPriorityDate] [date] NULL,
+	[AllocationExpirationDate] [date] NULL,
+	[AllocationOwner] [nvarchar](500) NULL,
+	[AllocationBasisCV] [nvarchar](250) NULL,
+	[AllocationLegalStatusCV] [varchar](250) NULL,
+	[AllocationTypeCV] [nvarchar](250) NULL,
+	[AllocationTimeframeStart] [nvarchar](6) NULL,
+	[AllocationTimeframeEnd] [nvarchar](6) NULL,
+	[AllocationCropDutyAmount] [float] NULL,
+	[AllocationFlow_CFS] [float] NULL,
+	[AllocationVolume_AF] [float] NULL,
+	[AllocationUUID] [nvarchar](250) NULL,
+	[PopulationServed] [bigint] NULL,
+	[GeneratedPowerCapacityMW] [float] NULL,
+	[IrrigatedAcreage] [float] NULL,
+	[AllocationCommunityWaterSupplySystem] [nvarchar](250) NULL,
+	[AllocationSDWISIdentifier] [nvarchar](250) NULL,
+	[AllocationAssociatedWithdrawalSiteIDs] [nvarchar](500) NULL,
+	[AllocationAssociatedConsumptiveUseSiteIDs] [nvarchar](500) NULL,
+	[AllocationChangeApplicationIndicator] [nvarchar](250) NULL,
+	[LegacyAllocationIDs] [nvarchar](250) NULL,
+	[CustomerType] [nvarchar](100) NULL,
+	[IrrigationMethodCV] [nvarchar](100) NULL,
+	[CropTypeCV] [nvarchar](100) NULL,
+	[WaterAllocationNativeURL] [nvarchar](250) NULL,
+	[CommunityWaterSupplySystem] [nvarchar](250) NULL,
+	[PowerType] [nvarchar](50) NULL,
+	[ExemptOfVolumeFlowPriority] [bit] NULL,
+	[OwnerClassificationCV] [nvarchar](250) NULL
+)
+GO
+
+PRINT 'created new table type'
+
+EXEC Core.UpdateUUDT 'Core', 'WaterAllocationTableType'
+GO
+PRINT 'replaced table type'
+
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
