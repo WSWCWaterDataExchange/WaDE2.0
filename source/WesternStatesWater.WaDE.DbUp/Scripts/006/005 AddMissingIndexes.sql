292f7f7fc4b736c9561a158c86736f7e22fd7a39
Nathan Wilkinson <nwilkinson@whatiffoundry.com>
2023-06-22 07:51:37 -0500

    Remove Create Index Already Recently Added

--- source/WesternStatesWater.WaDE.DbUp/Scripts/006/005 AddMissingIndexes.sql
+++ source/WesternStatesWater.WaDE.DbUp/Scripts/006/005 AddMissingIndexes.sql
@@ -3,11 +3,6 @@ ON [Core].[WaterSourceBridge_Sites_fact] ([SiteID])
 INCLUDE ([WaterSourceID])
 GO
 
-CREATE NONCLUSTERED INDEX IX_WaterSourceBridge_Sites_WaterSourceID
-ON [Core].[WaterSourceBridge_Sites_fact] ([WaterSourceID])
-INCLUDE ([SiteID])
-GO
-
 CREATE NONCLUSTERED INDEX IX_AllocationAmounts_AllocationPriorityDateID
 ON [Core].[AllocationAmounts_fact] ([AllocationPriorityDateID])
 GO
