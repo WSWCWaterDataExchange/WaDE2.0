7eee31a603afb6b01cf1ed241660f2ba0f876f6e
Zach Lannin <zlannin@dontpaniclabs.com>
2024-11-19 11:07:16 -0600

    Fixed state to be from organization and fixed bad water source join

--- source/WesternStatesWater.WaDE.DbUp/Scripts/007/001 CreateAllocationAmountsView.sql
+++ source/WesternStatesWater.WaDE.DbUp/Scripts/007/001 CreateAllocationAmountsView.sql
@@ -19,21 +19,21 @@ SELECT
     sd.SitePoint as [point]
 FROM (
     SELECT
-        sd.SiteID,
-        STRING_AGG(CAST(aaf.AllocationOwner as nvarchar(max)), '||') as o,
-        STRING_AGG(CAST(occv.WaDEName as nvarchar(max)), '||') as oClass,
-        STRING_AGG(CAST(bucv.WaDEName as nvarchar(max)), '||') as bu,
-        MIN(sd.SiteUUID) as uuid,
-        MIN(sd.PODorPOUSite) as podPou,
-        STRING_AGG(CAST(wstcv.WaDEName as nvarchar(max)), '||') as  wsType,
-        STRING_AGG(CAST(stcv.WaDEName as nvarchar(max)), '||') as st,
-        CAST(MIN(aaf.ExemptOfVolumeFlowPriority+0) as bit) as xmpt,
-        MIN(aaf.AllocationFlow_CFS) as minCfsFlow,
-        MAX(aaf.AllocationFlow_CFS) as maxCfsFlow,
-        MIN(aaf.AllocationVolume_AF) as minAfVolume,
-        MAX(aaf.AllocationVolume_AF) as maxAfVolume,
-        MIN(dd.[Date]) as minPriorityDate,
-        MAX(dd.[Date]) as maxPriorityDate
+    sd.SiteID,
+    STRING_AGG(CAST(aaf.AllocationOwner as nvarchar(max)), '||') as o,
+    STRING_AGG(CAST(occv.WaDEName as nvarchar(max)), '||') as oClass,
+    STRING_AGG(CAST(bucv.WaDEName as nvarchar(max)), '||') as bu,
+    MIN(sd.SiteUUID) as uuid,
+    MIN(sd.PODorPOUSite) as podPou,
+    STRING_AGG(CAST(wstcv.WaDEName as nvarchar(max)), '||') as  wsType,
+    STRING_AGG(CAST(od.[State] as nvarchar(max)), '||') as st,
+    CAST(MIN(aaf.ExemptOfVolumeFlowPriority+0) as bit) as xmpt,
+    MIN(aaf.AllocationFlow_CFS) as minCfsFlow,
+    MAX(aaf.AllocationFlow_CFS) as maxCfsFlow,
+    MIN(aaf.AllocationVolume_AF) as minAfVolume,
+    MAX(aaf.AllocationVolume_AF) as maxAfVolume,
+    MIN(dd.[Date]) as minPriorityDate,
+    MAX(dd.[Date]) as maxPriorityDate
     FROM Core.Sites_dim sd
     INNER JOIN Core.AllocationBridge_Sites_fact absf on absf.SiteID = sd.SiteID
     INNER JOIN Core.AllocationAmounts_fact aaf on aaf.AllocationAmountID = absf.AllocationAmountID
@@ -42,11 +42,10 @@ FROM (
     INNER JOIN Core.Organizations_dim od on aaf.OrganizationID = od.OrganizationID
     INNER JOIN Core.AllocationBridge_BeneficialUses_fact abbuf on abbuf.AllocationAmountID = aaf.AllocationAmountID
     LEFT OUTER JOIN CVs.BeneficialUses bucv on bucv.Name = abbuf.BeneficialUseCV
-    LEFT OUTER JOIN CVs.WaterSourceType wstcv on wstcv.Name = wsd.WaterSourceName
+    LEFT OUTER JOIN CVs.WaterSourceType wstcv on wstcv.Name = wsd.WaterSourceTypeCV
     LEFT OUTER JOIN CVs.OwnerClassification occv on occv.Name = aaf.OwnerClassificationCV
     LEFT OUTER JOIN CVs.CustomerType ctcv on ctcv.Name = aaf.CustomerTypeCV
-    LEFT OUTER JOIN CVs.SiteType stcv on stcv.Name = sd.SiteTypeCV
     LEFT OUTER JOIN Core.Date_dim dd on dd.DateID = aaf.AllocationPriorityDateID
-    GROUP BY sd.SiteID 
+    GROUP BY sd.SiteID
 ) as SA
 INNER JOIN Core.Sites_dim sd on sd.SiteID = SA.SiteID
