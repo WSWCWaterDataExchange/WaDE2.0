ee51c7b2aa95cb06edd73b3c87837a667f9d4582
Zach Lannin <zlannin@dontpaniclabs.com>
2024-12-19 16:51:20 -0600

    created OverlaySearchHandler

--- source/WesternStatesWater.WaDE.Accessors.Contracts.Api/Requests/OverlaySearchRequest.cs
+++ source/WesternStatesWater.WaDE.Accessors.Contracts.Api/Requests/OverlaySearchRequest.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using NetTopologySuite.Geometries;
 
 namespace WesternStatesWater.WaDE.Accessors.Contracts.Api.Requests;
 
 public class OverlaySearchRequest : SearchRequestBase
 {
+    public List<string> ReportingUnitUuids { get; set; }
     public List<string> OverlayUuids { get; set; }
     public List<string> SiteUuids { get; set; }
+    public Polygon FilterBoundary { get; set; }
     public string LastKey { get; set; }
 }
