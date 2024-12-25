43257e3da6b826e0acc98f3f7a01808030b20602
Andrew Tarr <andrew.tarr@hotmail.com>
2024-12-25 09:55:33 -0600

    Moving 'site variable amounts' search into the handler pattern

---
+++ source/WesternStatesWater.WaDE.Contracts.Api/Responses/V1/SiteVariableAmountsSearchResponse.cs
@@ -0,0 +1,6 @@
+namespace WesternStatesWater.WaDE.Contracts.Api.Responses.V1;
+
+public class SiteVariableAmountsSearchResponse : WaterResourceLoadResponseBase
+{
+    public SiteVariableAmounts SiteVariableAmounts { get; init; }
+}
--- source/WesternStatesWater.WaDE.Managers.Tests/Handlers/V1/SiteVariableAmountsSearchRequestHandlerTests.cs
+++ source/WesternStatesWater.WaDE.Managers.Tests/Handlers/V1/SiteVariableAmountsSearchRequestHandlerTests.cs
@@ -1,34 +1,44 @@
-﻿using System.Linq;
+using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Telerik.JustMock;
 using Telerik.JustMock.Helpers;
 using WesternStatesWater.WaDE.Contracts.Api;
+using WesternStatesWater.WaDE.Contracts.Api.Requests.V1;
+using WesternStatesWater.WaDE.Managers.Api.Handlers.V1;
 using WesternStatesWater.WaDE.Tests.Helpers.ModelBuilder.Accessor.Api;
 using WesternStatesWater.WaDE.Utilities;
 
-namespace WesternStatesWater.WaDE.Managers.Tests.WaterResourceManager
+namespace WesternStatesWater.WaDE.Managers.Tests.Handlers.V1;
+
+public class SiteVariableAmountsSearchRequestHandlerTests : HandlerTestsBase
 {
-    [TestClass]
-    public class ApiSiteVariableAmountsManagerTests : WaterResourceManagerTestsBase
-    {
         [DataTestMethod]
         [DataRow(null, GeometryFormat.Wkt, null)]
         [DataRow(null, GeometryFormat.GeoJson, null)]
         [DataRow("POINT (-96.7014 40.8146)", GeometryFormat.Wkt, "POINT (-96.7014 40.8146)")]
         [DataRow("POINT (-96.7014 40.8146)", GeometryFormat.GeoJson, "{\"type\":\"Point\",\"coordinates\":[-96.7014,40.8146]}")]
-        public async Task GetSiteAllocationAmountsAsync_Site_SiteGeometries(string geometryString, GeometryFormat geometryFormat, string expectedResultString)
+        public async Task GetSiteVariableAmountsAsync_Site_SiteGeometries(string geometryString, GeometryFormat geometryFormat, string expectedResultString)
         {
             var accessorResult = SiteVariableAmountsBuilder.Create();
             accessorResult.Organizations.First().Sites[0].SiteGeometry = GeometryExtensions.GetGeometryByWkt(geometryString);
             SiteVariableAmountsAccessorMock.Arrange(a => a.GetSiteVariableAmountsAsync(Arg.IsAny<Accessors.Contracts.Api.SiteVariableAmountsFilters>(), 0, 1))
                                        .Returns(Task.FromResult(accessorResult));
-            var sut = CreateSiteVariableAmountsManager();
-            var result = await sut.GetSiteVariableAmountsAsync(new Contracts.Api.SiteVariableAmountsFilters(), 0, 1, geometryFormat);
+            var sut = CreateHandler();
+
+            var request = new SiteVariableAmountsSearchRequest
+            {
+                Filters = new SiteVariableAmountsFilters(),
+                StartIndex = 0,
+                RecordCount = 1,
+                OutputGeometryFormat = geometryFormat
+            };
+
+            var result = await sut.Handle(request);
             result.Should().NotBeNull();
 
-            var resultGeometry = result.Organizations.First().Sites[0].SiteGeometry;
+            var resultGeometry = result.SiteVariableAmounts.Organizations.First().Sites[0].SiteGeometry;
             if (expectedResultString == null)
             {
                 resultGeometry.Should().BeNull();
@@ -45,17 +55,27 @@ namespace WesternStatesWater.WaDE.Managers.Tests.WaterResourceManager
         [DataRow(null, GeometryFormat.GeoJson, null)]
         [DataRow("POINT (-96.7014 40.8146)", GeometryFormat.Wkt, "POINT (-96.7014 40.8146)")]
         [DataRow("POINT (-96.7014 40.8146)", GeometryFormat.GeoJson, "{\"type\":\"Point\",\"coordinates\":[-96.7014,40.8146]}")]
-        public async Task GetSiteAllocationAmountsAsync_WaterSource_WaterSourceGeometry(string geometryString, GeometryFormat geometryFormat, string expectedResultString)
+        public async Task GetSiteVariableAmountsAsync_WaterSource_WaterSourceGeometry(string geometryString, GeometryFormat geometryFormat, string expectedResultString)
         {
             var accessorResult = SiteVariableAmountsBuilder.Create();
             accessorResult.Organizations.First().WaterSources[0].WaterSourceGeometry = GeometryExtensions.GetGeometryByWkt(geometryString);
             SiteVariableAmountsAccessorMock.Arrange(a => a.GetSiteVariableAmountsAsync(Arg.IsAny<Accessors.Contracts.Api.SiteVariableAmountsFilters>(), 0, 1))
                                        .Returns(Task.FromResult(accessorResult));
-            var sut = CreateSiteVariableAmountsManager();
-            var result = await sut.GetSiteVariableAmountsAsync(new Contracts.Api.SiteVariableAmountsFilters(), 0, 1, geometryFormat);
+            var sut = CreateHandler();
+            
+            var request = new SiteVariableAmountsSearchRequest
+            {
+                Filters = new SiteVariableAmountsFilters(),
+                StartIndex = 0,
+                RecordCount = 1,
+                OutputGeometryFormat = geometryFormat
+            };
+            
+            var result = await sut.Handle(request);
+            
             result.Should().NotBeNull();
 
-            var resultGeometry = result.Organizations.First().WaterSources[0].WaterSourceGeometry;
+            var resultGeometry = result.SiteVariableAmounts.Organizations.First().WaterSources[0].WaterSourceGeometry;
             if (expectedResultString == null)
             {
                 resultGeometry.Should().BeNull();
@@ -67,9 +87,8 @@ namespace WesternStatesWater.WaDE.Managers.Tests.WaterResourceManager
             }
         }
 
-        private ISiteVariableAmountsManager CreateSiteVariableAmountsManager()
-        {
-            return CreateWaterResourceManager();
-        }
+    private SiteVariableAmountsSearchRequestHandler CreateHandler()
+    {
+        return new SiteVariableAmountsSearchRequestHandler(SiteVariableAmountsAccessorMock);
     }
 }
