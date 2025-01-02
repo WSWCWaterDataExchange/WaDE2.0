36bf4961b97c08fe1755f1959a8f968383c4886f
Zach Lannin <zlannin@dontpaniclabs.com>
2025-01-02 16:59:20 -0600

    moved ISiteManager to IWaterResourceManager

--- source/WesternStatesWater.WaDE.Contracts.Api/IWaterResourceManager.cs
+++ source/WesternStatesWater.WaDE.Contracts.Api/IWaterResourceManager.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
 using WesternStatesWater.WaDE.Contracts.Api.Requests;
+using WesternStatesWater.WaDE.Contracts.Api.Requests.V2;
 using WesternStatesWater.WaDE.Contracts.Api.Responses;
+using WesternStatesWater.WaDE.Contracts.Api.Responses.V2;
 
 namespace WesternStatesWater.WaDE.Contracts.Api;
 
@@ -9,4 +11,8 @@ public interface IWaterResourceManager
     Task<TResponse> Load<TRequest, TResponse>(TRequest request)
         where TRequest : WaterResourceLoadRequestBase
         where TResponse : WaterResourceLoadResponseBase;
+    
+    public Task<TResponse> Search<TRequest, TResponse>(TRequest request)
+        where TRequest : FeaturesSearchRequestBase
+        where TResponse : FeaturesSearchResponseBase;
 }
