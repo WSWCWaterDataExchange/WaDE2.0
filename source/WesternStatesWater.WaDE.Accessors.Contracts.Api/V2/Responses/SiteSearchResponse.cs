98203ed1215a069d5faf1d8e5a5d196fe751c3c5
Zach Lannin <zlannin@dontpaniclabs.com>
2025-01-03 11:27:37 -0600

    added SiteFeaturesRequestHandlerTests

--- source/WesternStatesWater.WaDE.Accessors.Contracts.Api/V2/Responses/SiteSearchResponse.cs
+++ source/WesternStatesWater.WaDE.Accessors.Contracts.Api/V2/Responses/SiteSearchResponse.cs
@@ -4,5 +4,5 @@ namespace WesternStatesWater.WaDE.Accessors.Contracts.Api.V2.Responses;
 
 public class SiteSearchResponse : SearchResponseBase
 {
-    public List<SiteSearchItem> Sites { get; set; }
+    public List<SiteSearchItem> Sites { get; set; } = [];
 }
