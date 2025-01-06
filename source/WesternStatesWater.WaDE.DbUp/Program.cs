cdaa794c219e4bcfc913c8dae5962063ae3e1826
Ethan Weber <eweber@dontpaniclabs.com>
2025-01-06 15:35:26 -0600

    update backend connection strings

--- source/WesternStatesWater.WaDE.DbUp/Program.cs
+++ source/WesternStatesWater.WaDE.DbUp/Program.cs
@@ -17,7 +17,7 @@ namespace WesternStatesWater.WaDE.DbUp
                 var builder = new ConfigurationBuilder()
                     .AddInMemoryCollection(new Dictionary<string, string>
                     {
-                        { "ConnectionStrings:WadeDatabase", "Server=.;Initial Catalog=WaDE2;Integrated Security=true;" }
+                        { "ConnectionStrings:WadeDatabase", "Server=localhost;Initial Catalog=WaDE2Test;TrustServerCertificate=True;User=sa;Password=DevP@ssw0rd!;Encrypt=False;" }
                     })
                     .AddUserSecrets("0233c5d4-6e7e-4ba4-997b-313518edcce4")
                     .AddEnvironmentVariables();
