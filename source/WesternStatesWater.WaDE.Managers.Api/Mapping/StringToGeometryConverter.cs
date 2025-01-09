134ef6b4bcf7e8df4e3c5751ba0714d39818b190
Zach Lannin <zlannin@dontpaniclabs.com>
2025-01-09 09:21:37 -0600

    moved hard coded SRID into a constant to better indicate what the number represents

--- source/WesternStatesWater.WaDE.Managers.Api/Mapping/StringToGeometryConverter.cs
+++ source/WesternStatesWater.WaDE.Managers.Api/Mapping/StringToGeometryConverter.cs
@@ -5,6 +5,8 @@ namespace WesternStatesWater.WaDE.Managers.Api.Mapping;
 
 public class StringToGeometryConverter : IValueConverter<string, Geometry>
 {
+    // Spatial Reference ID for WGS84
+    private const int SRID_WGS84 = 4326;
     public Geometry Convert(string sourceMember, ResolutionContext context)
     {
         if (sourceMember == null)
@@ -12,7 +14,7 @@ public class StringToGeometryConverter : IValueConverter<string, Geometry>
             return null;
         }
 
-        var reader = new NetTopologySuite.IO.WKTReader(new NetTopologySuite.NtsGeometryServices(PrecisionModel.Floating.Value, 4326));
+        var reader = new NetTopologySuite.IO.WKTReader(new NetTopologySuite.NtsGeometryServices(PrecisionModel.Floating.Value, SRID_WGS84));
         return reader.Read(sourceMember);
     }
 }
