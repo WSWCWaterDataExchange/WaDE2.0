f22687514550591c9ef04bb4ffac029b69d4cbaf
Zach Lannin <zlannin@dontpaniclabs.com>
2025-01-09 15:36:41 -0600

    Updated SearchResponsebase to have MatchedCount and LastUuid

--- source/WesternStatesWater.WaDE.Accessors.Contracts.Api/V2/Responses/SearchResponseBase.cs
+++ source/WesternStatesWater.WaDE.Accessors.Contracts.Api/V2/Responses/SearchResponseBase.cs
@@ -4,4 +4,6 @@ namespace WesternStatesWater.WaDE.Accessors.Contracts.Api.V2.Responses;
 
 public abstract class SearchResponseBase : ResponseBase
 {
+    public int MatchedCount { get; set; }
+    public string LastUuid { get; set; }
 }
