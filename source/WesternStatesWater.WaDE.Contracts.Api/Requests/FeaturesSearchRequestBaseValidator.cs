9babc25cc132d986c93cf78024fb1605d8683da4
Zach Lannin <zlannin@dontpaniclabs.com>
2025-01-09 10:10:25 -0600

    moved FeatureSearchRequestBase validators to its own validator

---
+++ source/WesternStatesWater.WaDE.Contracts.Api/Requests/FeaturesSearchRequestBaseValidator.cs
@@ -0,0 +1,22 @@
+using FluentValidation;
+using WesternStatesWater.WaDE.Contracts.Api.Extensions;
+
+namespace WesternStatesWater.WaDE.Contracts.Api.Requests;
+
+public class FeaturesSearchRequestBaseValidator : AbstractValidator<FeaturesSearchRequestBase>
+{
+    public FeaturesSearchRequestBaseValidator()
+    {
+        When(req => req.Bbox != null, () =>
+        {
+            RuleFor(req => req.Bbox)
+                .Bbox()
+                .DependentRules(() => RuleFor(req => req.Bbox).BboxInRange());
+        });
+
+        When(req => req.Limit != null, () =>
+        {
+            RuleFor(req => req.Limit).GreaterThan(1);
+        });
+    }
+}
--- source/WesternStatesWater.WaDE.Contracts.Api/Requests/V2/OverlayFeaturesSearchRequestValidator.cs
+++ source/WesternStatesWater.WaDE.Contracts.Api/Requests/V2/OverlayFeaturesSearchRequestValidator.cs
@@ -1,5 +1,4 @@
 using FluentValidation;
-using WesternStatesWater.WaDE.Contracts.Api.Extensions;
 
 namespace WesternStatesWater.WaDE.Contracts.Api.Requests.V2;
 
@@ -7,16 +6,6 @@ public class OverlayFeaturesSearchRequestValidator : AbstractValidator<OverlayFe
 {
     public OverlayFeaturesSearchRequestValidator()
     {
-        When(req => req.Bbox != null, () =>
-        {
-            RuleFor(req => req.Bbox)
-                .Bbox()
-                .DependentRules(() => RuleFor(req => req.Bbox).BboxInRange());
-        });
-
-        When(req => req.Limit != null, () =>
-        {
-            RuleFor(req => req.Limit).LimitInRange(1, 1_000);
-        });
+        Include(new FeaturesSearchRequestBaseValidator());
     }
 }
