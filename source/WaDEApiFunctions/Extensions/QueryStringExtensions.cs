8f2f4e4eadd2815b266e35d0713cf762dffddfaf
Zach Lannin <zlannin@dontpaniclabs.com>
2025-02-06 15:48:58 -0600

    /items and /area endpoints return 400 if invalid parameter is passed

---
+++ source/WaDEApiFunctions/Extensions/QueryStringExtensions.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using WesternStatesWater.Shared.DataContracts;
+
+namespace WaDEApiFunctions.Extensions;
+
+public static class QueryStringExtensions
+{
+    public static (bool, List<string>) ContainsUnmatchedParameters<T>(this NameValueCollection query) where T : RequestBase
+    {
+        var properties = typeof(T).GetProperties();
+        var unmatchedParameters = new List<string>();
+
+        foreach (var key in query.AllKeys)
+        {
+            if (properties.All(p => !p.Name.Equals(key, StringComparison.InvariantCultureIgnoreCase)))
+            {
+                unmatchedParameters.Add(key);
+            }
+        }
+
+        return (unmatchedParameters.Any(), unmatchedParameters);
+    }
+}
