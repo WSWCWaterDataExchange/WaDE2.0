31fc9f5ebb40a583e277604a8d03c68b76d6b1d9
Zach Lannin <zlannin@dontpaniclabs.com>
2025-02-06 14:47:48 -0600

    marked IOgcFeature Type and Id as nullable

--- source/WesternStatesWater.WaDE.Common/Ogc/IOgcFeature.cs
+++ source/WesternStatesWater.WaDE.Common/Ogc/IOgcFeature.cs
@@ -5,8 +5,8 @@ namespace WesternStatesWater.WaDE.Common.Ogc;
 
 public interface IOgcFeature
 {
-    public string Type { get; }
-    public string Id { get; set; }
+    public string? Type { get; }
+    public string? Id { get; set; }
     public Geometry? Geometry { get; set; }
     public AttributesTable? Properties { get; set; }
     public Link[]? Links { get; set; }
